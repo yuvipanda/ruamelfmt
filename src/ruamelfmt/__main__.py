de0f29c369da811b1580e25d2c42e37728ce3557
YuviPanda <yuvipanda@gmail.com>
2025-05-23 17:22:49 -0700

    Print errors more cleanly

--- src/ruamelfmt/__main__.py
+++ src/ruamelfmt/__main__.py
@@ -1,6 +1,8 @@
 import argparse
 import sys
 from ruamel.yaml import YAML
+from ruamel.yaml.constructor import DuplicateKeyError
+from ruamel.yaml.scanner import ScannerError
 
 def main():
     parser = argparse.ArgumentParser()
@@ -11,12 +13,22 @@ def main():
     yaml = YAML(typ='rt')
     yaml.width = args.line_length
 
+    should_fail = False
     for filepath in args.filepath:
         with open(filepath) as f:
-            data = yaml.load(f)
+            try:
+                data = yaml.load(f)
+            except (ScannerError, DuplicateKeyError) as e:
+                print(f"Error {str(e.problem_mark).strip()}: {e.problem.strip()}")
+                should_fail = True
+                # Continue formatting other files
+                continue
 
         with open(filepath, 'w') as f:
             yaml.dump(data, f)
+
+    if should_fail:
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
