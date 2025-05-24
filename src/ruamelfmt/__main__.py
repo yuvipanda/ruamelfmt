e207430741d687d877ffc16201cce47efd2f96dc
YuviPanda <yuvipanda@gmail.com>
2025-05-23 17:02:59 -0700

    Support multiple file paths

--- src/ruamelfmt/__main__.py
+++ src/ruamelfmt/__main__.py
@@ -4,17 +4,18 @@ from ruamel.yaml import YAML
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--line-length', default=120, type=int)
-    parser.add_argument('filepath')
+    parser.add_argument('filepath', nargs='+')
     args = parser.parse_args()
 
     yaml = YAML(typ='rt')
     yaml.width = args.line_length
 
-    with open(args.filepath) as f:
-        data = yaml.load(f)
+    for filepath in args.filepath:
+        with open(filepath) as f:
+            data = yaml.load(f)
 
-    with open(args.filepath, 'w') as f:
-        yaml.dump(data, f)
+        with open(filepath, 'w') as f:
+            yaml.dump(data, f)
 
 if __name__ == '__main__':
     main()
